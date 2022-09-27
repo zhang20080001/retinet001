109eba99fe10abda2fbdab7ff7246b370f8e6474
zhang20080001 <51077136+zhang20080001@users.noreply.github.com>
2022-09-27 11:28:39 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ parser = argparse.ArgumentParser(description='')
 
 parser.add_argument('--use_gpu', dest='use_gpu', type=int, default=1, help='gpu flag, 1 for GPU and 0 for CPU')
 parser.add_argument('--gpu_idx', dest='gpu_idx', default="0", help='GPU idx')
-parser.add_argument('--gpu_mem', dest='gpu_mem', type=float, default=0.5, help="0 to 1, gpu memory usage")
+parser.add_argument('--gpu_mem', dest='gpu_mem', type=float, default=1, help="0 to 1, gpu memory usage")
 parser.add_argument('--phase', dest='phase', default='train', help='train or test')
 
 parser.add_argument('--epoch', dest='epoch', type=int, default=100, help='number of total epoches')
